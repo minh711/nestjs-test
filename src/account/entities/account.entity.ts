3b8ae7a97038edc98d506b3dec24d8875d57ce97
minh711 <duyminhtruong711@gmail.com>
2024-02-13 22:31:22 +0700

    Integrate `TypeORM` with `Account` entity.

--- src/account/entities/account.entity.ts
+++ src/account/entities/account.entity.ts
@@ -1,31 +1,46 @@
 import { ObjectType, Field, Int } from '@nestjs/graphql';
+import {
+  Column,
+  CreateDateColumn,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
 @ObjectType()
 export class Account {
+  @PrimaryGeneratedColumn()
   @Field(() => Int)
   id: number;
 
+  @Column()
   @Field(() => String)
   username: string;
 
+  @Column()
   @Field(() => String)
   email: string;
 
+  @Column()
   @Field({ nullable: true })
   firstName?: string;
 
+  @Column()
   @Field({ nullable: true })
   lastName?: string;
 
+  @Column()
   @Field({ nullable: true })
   gender?: string;
 
+  @Column()
   @Field(() => Int, { nullable: true })
   age?: number;
 
+  @CreateDateColumn()
   @Field(() => Date)
   createdAt: Date;
 
+  @UpdateDateColumn()
   @Field(() => Date)
   updatedAt: Date;
 }
