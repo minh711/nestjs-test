fcb1826c3c03f40cd0a2ef393a740cbe1ce4036b
minh711 <duyminhtruong711@gmail.com>
2024-02-14 08:14:24 +0700

    Move files.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AccountModule } from './account/account.module';
+import { AccountModule } from '../account/account.module';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver } from '@nestjs/apollo';
 import { join } from 'path';
