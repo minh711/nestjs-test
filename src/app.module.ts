488f34b1a85c8f098c0bdda1a411c610d36c9e9f
minh711 <duyminhtruong711@gmail.com>
2024-02-15 09:09:04 +0700

    Re-arrange files.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AccountModule } from '../account/account.module';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver } from '@nestjs/apollo';
 import { join } from 'path';
 import { DatabaseModule } from './databases/database.module';
 import { ConfigModule } from '@nestjs/config';
+import { AccountModule } from './account/account.module';
 
 @Module({
   imports: [
